61c5f97d795899e5875dedc0beba0b8c641cdf57
MousaHussen <Mousahusseen@gmail.com>
2022-07-04 20:32:11 +0300

    add components and router for it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+// Import the page
+import Home from "./component/Home";
+import Favorite from "./component/Favorite";
+import Movie from "./component/Movie";
+import Navbar from "./component/Navbar";
 
 function App() {
-  return <div className="App">Welcome APP</div>;
+  return <div className="containerMovie">
+    <Navbar/>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/favorite" element={<Favorite/>}/>
+    <Route path="/0" element={<Movie/>}/>
+  </Routes>
+  </div>
 }
 
 export default App;
