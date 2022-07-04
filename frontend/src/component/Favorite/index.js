fa815557aec70e130338b165d8c84eab8544668f
MousaHussen <Mousahusseen@gmail.com>
2022-07-04 20:33:14 +0300

    the components and styles

---
+++ frontend/src/component/Favorite/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css"
+const Favorite = () => {
+  return (
+    <div>
+      <h1>Favorite page</h1>
+    </div>
+  );
+};
+
+export default Favorite;
---
+++ frontend/src/component/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1 id="dana">Hoome</h1>
+    </div>
+  );
+};
+
+export default Home;
