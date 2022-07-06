50d83ec852f5af6ea6a61e3674444a58b0ab3ca2
MousaHussen <Mousahusseen@gmail.com>
2022-07-06 12:32:31 +0300

    adding to local storage favorit and styling the page

--- frontend/src/component/Favorite/index.js
+++ frontend/src/component/Favorite/index.js
@@ -1,10 +1,65 @@
-import React from "react";
-import "./style.css"
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import "./style.css";
 const Favorite = () => {
+  let store = JSON.parse(localStorage.getItem("Fav"));
+
+  const [favorite, setFavorite] = useState([]);
+
+  const getFavoriteMovie = () => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/popular?api_key=1bfa430aada4409bfa6a3c5528128e8a"
+      )
+      .then((result) => {
+        setFavorite(result.data.results);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
+
+const deleteFav = (element)=>{
+ 
+store =  store.filter((elem,index)=>{
+    
+    return elem != element
+  })
+  console.log(store);
+  
+}
+
+  useEffect(() => {
+    getFavoriteMovie();
+    deleteFav()
+  }, []);
+console.log(store);
   return (
-    <div>
-      <h1>Favorite page</h1>
-    </div>
+    
+    <div className="contanierFav">
+      {store
+        ? store.map((element, index) => {
+            //console.log(element);
+            return (
+              <div className="pMovie" key={index}>
+              <a href={`/movie/${element.id}`}>
+                <img
+                  className="mosa"
+                  src={`http://image.tmdb.org/t/p/w500/${element.poster_path}`}
+                  key={element.id}
+                />
+              </a>
+              <button onClick={()=>{
+                deleteFav(element)
+              }}>X</button>
+            </div>
+              
+            );
+          })
+        : []}
+        </div>
+    
   );
 };
 
